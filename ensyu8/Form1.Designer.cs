c47c3bbf6b6214de6cdc120b9020fc8f3b8d0d77
pizza_girl <pizza.girl.dat@gmail.com>
2020-08-06 10:53:14 +0900

    演習8

--- ensyu8/Form1.Designer.cs
+++ ensyu8/Form1.Designer.cs
@@ -29,11 +29,42 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(45, 78);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(37, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "(・ω・)";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(510, 435);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Timer timer1;
     }
 }
 
--- ensyu8/Form1.cs
+++ ensyu8/Form1.cs
@@ -12,9 +12,57 @@ namespace ensyu8
 {
     public partial class Form1 : Form
     {
+        int vx=-10;
+        int vy=-10;
+        string kao = "(・◇・)";
+        
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+                label1.Left += vx;
+                label1.Top += vy;
+
+                if (label1.Left < 0)
+                {
+                    vx = 10;
+                }
+
+                else if (label1.Top < 0)
+                {
+                    vy = 10;
+                }
+
+                else if (label1.Right > 0)
+                {
+                    vx = 10;
+                }
+
+                else if (label1.Bottom > 0)
+                {
+                    vx = 10;
+                }
+
+                string t = label1.Text;
+                label1.Text = kao;
+                kao = t;
+            
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            vx = 0;
+            vy = 0;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //MessageBox.Show("幅" + ClientSize.Width);
+            //MessageBox.Show("高さ" + ClientSize.Width);
+           
         }
     }
 }
